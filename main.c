1a320277bd94f85f7b5ebcb8089b69d43696120e
kolserdav <serega12101983@gmail.com>
2023-02-21 02:47:27 +0700

    listen socket

--- main.c
+++ main.c
@@ -1,23 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h>
-
-#define SEC_TO_US(sec) ((sec)*1000000)
-#define NS_TO_US(ns)    ((ns)/1000)
-
-uint64_t micros()
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h> // read(), write(), close()
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+   
+void func(int connfd)
 {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-    uint64_t us = SEC_TO_US((uint64_t)ts.tv_sec) + NS_TO_US((uint64_t)ts.tv_nsec);
-    return us;
+    char buff[MAX];
+    for (;;) {
+        bzero(buff, MAX);
+ 	char heads[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nHost:localhost:8080\r\nContent-Length:3\r\n\r\nhhh";  
+        write(connfd, heads, sizeof(heads));
+    }
 }
-
-int main(void) { 
-	char s[] = "Hello from C!";
-	uint64_t start = micros();
-	puts(s);
-	printf("%lu\n",micros() - start);
- 	return EXIT_SUCCESS;
-} 
-
+   
+// Driver function
+int main()
+{
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+   
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+   
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+   
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded..\n");
+   
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
+    len = sizeof(cli);
+   
+    connfd = accept(sockfd, (SA*)&cli, &len);
+    if (connfd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server accept the client...\n");
+   
+    func(connfd);
+   
+}
